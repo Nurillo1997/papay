bd1ed098ae6f65564ad04c2e626b2491b52e083d
Nurillo1997 <nurilloabdurazzoqov@gmail.com>
2023-05-29 09:58:49 +0900

    fix: building mongoose connection and schema model

--- server.js
+++ server.js
@@ -3,22 +3,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const http = require("http");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
 // MongoDB connect
 
 const connectionString = process.env.MONGO_URL;
-mongodb.connect(connectionString,
+mongoose.connect(connectionString,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     },
-    (err, client) => {
+    (err, goose) => {
         if (err) console.log("ERROR on connection MongoDB");
         else {
             console.log("MongoDB connection succeed");
-            module.exports = client;
-
             const app = require("./app");
             const server = http.createServer(app);
             let PORT = process.env.PORT || 3003;

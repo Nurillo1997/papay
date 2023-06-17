1ae14a5534808565cae94a60e4adfbd56435cd7d
Nurillo1997 <nurilloabdurazzoqov@gmail.com>
2023-06-17 13:47:40 +0900

    feat: adminka signup page publishing

--- router.js
+++ router.js
@@ -7,7 +7,7 @@ const memberController = require("./controllers/memberController");
  *****************************************/
 
 // memberga dahldor routerlar
-router.post("/signup",  memberController.signup);
+router.post("/sign-up",  memberController.signup);
 router.post("/login",  memberController.login);
 router.get("/logout",  memberController.logout);
 
